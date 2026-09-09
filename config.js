473649758b3f83f4a3601c530c3e474d944ee786
myeolun36-debug <myeolun36@gmail.com>
2026-09-09 23:55:07 +0800

    k

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // ⚠️ 部署完 Google Apps Script 之後,把拿到的網址貼在這裡(引號中間),
 // 存檔後重新部署到 Netlify(或直接覆蓋這個檔案),網頁就會抓到真的資料。
 // 網址長得像:https://script.google.com/macros/s/AKfycb.../exec
-window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzjLKbZZV1vMvD4B4zsOKVcJwlT1JFSGKezGei2PKyUBUaJHguNvPKUPN0dfOcZptknAg/execPASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzU51Rudt-XsH6W525XZsVme6HC5x0KUh9Q1mMP5ClhqC_JshLg5tua68j9JL4jeasw9A/exec";
