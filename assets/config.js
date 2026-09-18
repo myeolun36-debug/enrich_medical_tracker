4b6b1158ff81b5f020c81e783f407c3eb3a255cd
myeolun36-debug <myeolun36@gmail.com>
2026-09-18 16:13:34 +0800

    open your eyes

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,4 @@
 // ⚠️ 部署完 Google Apps Script 之後,把拿到的網址貼在這裡(引號中間),
 // 存檔後重新部署到 Netlify(或直接覆蓋這個檔案),網頁就會抓到真的資料。
 // 網址長得像:https://script.google.com/macros/s/AKfycb.../exec
-window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzo9uRj4X1xLf_IuSSFCQ_6HtCEGwd9Z-KMyQcyWsVOTd6rD-gWwYD_gZH4U6bMLq7Vlw/exec";
+window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzo9uRj4X1xLf_IuSSFCQ_6HtCEGwd9Z-KMyQcyWsVOTd6rD-gWwYD_gZH4U6bMLq7Vlw/exechttps://script.google.com/macros/s/AKfycbxPR4huzrdguGMejgfeiCzqfIBeiJQEr5u9B3_Amzq8BYUcgY3eEQ6bqLrB6rDZxxwP6g/exec";
